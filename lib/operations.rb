28869409230402e2ec6b1db031894b8092ef2634
Sierra McCahon <smitche4@mail.umw.edu>
2018-03-12 02:18:02 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,6 +1,5 @@
 
 def unsafe?(speed)
-  speed = gets.to_i
   if speed <= 40 || speed >= 60
     return true
   else
@@ -11,8 +10,6 @@ end
 
 
 def not_safe?(speed)
-	speed = gets.to_i
-
 	speed < 40 ? true : false || speed > 60 ? true : false
 end
 	
