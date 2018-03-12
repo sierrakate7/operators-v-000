10f19bfd0f06afd80445311bb3afccb72dc5c3b9
Sierra McCahon <smitche4@mail.umw.edu>
2018-03-12 02:18:33 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,16 @@
-def unsafe?(speed)
 
+def unsafe?(speed)
+  if speed <= 40 || speed >= 60
+    return true
+  else
+    return false
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+	speed < 40 ? true : false || speed > 60 ? true : false
 end
 	
 
